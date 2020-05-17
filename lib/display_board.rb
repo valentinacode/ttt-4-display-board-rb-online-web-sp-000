b3dc5b0bdc26d0e1c64fbc63736603c9eaf01f77
Valentina E <tiyahelfrih@gmail.com>
2020-05-17 17:30:50 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@ def display_board(board)
 puts "board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]"
 
 
-puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+puts ": #{board[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 
 ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 end
