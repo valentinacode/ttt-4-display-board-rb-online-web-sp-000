21eef22f267e9ccbef5baf30939d2711c2a16d1c
Valentina E <tiyahelfrih@gmail.com>
2020-05-17 17:27:19 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,6 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(board)
+puts "board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]"
+puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+end
+
+display_board(board)
